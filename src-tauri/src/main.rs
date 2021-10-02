23e38a07c3001efe3368bd64565f74e756895e7a
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2021-10-02 16:23:45 +0300

    example run command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,19 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
-
+use std::process::Command;
+#[tauri::command]
+fn my_custom_command() {
+  Command::new("sh")
+            .arg("-c")
+            .arg("manjaro-application-utility")
+            .output()
+            .expect("failed to execute process");
+}
 fn main() {
   tauri::Builder::default()
+  // This is where you pass in your commands
+  .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
