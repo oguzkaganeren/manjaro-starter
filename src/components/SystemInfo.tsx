9f64a7e6230f75671c1bddea1e0c0b6092a475cc
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-07-31 17:18:46 +0300

    system info design started

--- src/components/SystemInfo.tsx
+++ src/components/SystemInfo.tsx
@@ -6,31 +6,66 @@ import {
   TagLabel,
   useColorModeValue,
   chakra,
-  Text,
-  Tag,
-  IconButton,
-  useToast,
-  Spacer,
+  SimpleGrid,
+  Stat,
+  StatLabel,
+  StatNumber,
 } from '@chakra-ui/react';
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, {
+  ReactNode, useState, useEffect, useLayoutEffect,
+} from 'react';
+import { FiCpu } from 'react-icons/fi';
 import { invoke } from '@tauri-apps/api/tauri';
 
+interface StatsCardProps {
+  title: string;
+  stat: string;
+  icon: ReactNode;
+}
 interface SystemInfoComponentProps {
 }
-
+function StatsCard(props: StatsCardProps) {
+  const { title, stat, icon } = props;
+  return (
+    <Stat
+      px={{ base: 2, md: 4 }}
+      py="5"
+      shadow="xl"
+      border="1px solid"
+      borderColor={useColorModeValue('gray.800', 'gray.500')}
+      rounded="lg"
+    >
+      <Flex justifyContent="space-between">
+        <Box pl={{ base: 2, md: 4 }}>
+          <StatLabel fontWeight="medium" isTruncated>
+            {title}
+          </StatLabel>
+          <StatNumber fontSize="2xl" fontWeight="medium">
+            {stat}
+          </StatNumber>
+        </Box>
+        <Box
+          my="auto"
+          color={useColorModeValue('gray.800', 'gray.200')}
+          alignContent="center"
+        >
+          {icon}
+        </Box>
+      </Flex>
+    </Stat>
+  );
+}
 const SystemInfoComponent: React.FC<SystemInfoComponentProps> = (props) => {
   const [systemInfo, setSystemInfo] = useState({ numberOfCpu: '' });
   useEffect(() => {
     invoke('get_sys_info').then((response) => {
       // why two parse???
       const responseJson = JSON.parse(JSON.parse(JSON.stringify(response)));
-      console.log(typeof responseJson);
       setSystemInfo(responseJson);
     });
   }, []);
   return (
     <Box mt={5} mb={5} textAlign={{ lg: 'left' }}>
-
       <chakra.p
         mt={2}
         mb={4}
@@ -42,7 +77,14 @@ const SystemInfoComponent: React.FC<SystemInfoComponentProps> = (props) => {
       >
         System Details
       </chakra.p>
-      {systemInfo.numberOfCpu}
+      <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 5, lg: 8 }}>
+        <StatsCard
+          title="CPU"
+          stat={`${systemInfo.numberOfCpu} Core`}
+          icon={<FiCpu size="3em" />}
+        />
+      </SimpleGrid>
+
     </Box>
   );
 };
