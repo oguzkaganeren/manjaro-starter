aef26d734e99f31c73058407ccf42d8f38d1963a
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-09-30 23:23:46 +0300

    fix: 🐛 react tour fixes
    react tour problem fixes when click next button
    
    ✅ Closes: #65

--- src/components/StepButtons.tsx
+++ src/components/StepButtons.tsx
@@ -1,5 +1,6 @@
 import { Button, Flex } from '@chakra-ui/react';
-
+import { useEffect } from 'react';
+import { useTour } from '@reactour/tour';
 import { useTranslation } from 'react-i18next';
 
 type StepButtonsProps = {
@@ -18,6 +19,12 @@ const StepButtons = ({
   isLast,
 }: StepButtonsProps): JSX.Element => {
   const { t } = useTranslation();
+  const { setIsOpen } = useTour();
+  useEffect(() => {
+    if (!prevDisabled) {
+      setIsOpen(false);
+    }
+  }, [prevDisabled]);
   const nextButtonText = () => {
     if (prevDisabled) {
       return t('start');
