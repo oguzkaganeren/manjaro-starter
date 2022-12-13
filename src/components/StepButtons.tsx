41fc7728f081312e502b77aeb16f3fb5e531927d
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-12-13 19:28:14 +0300

    fix: 🐛 removed react tour(no need)
    removed react tour

--- src/components/StepButtons.tsx
+++ src/components/StepButtons.tsx
@@ -1,6 +1,4 @@
 import { Button, Flex } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { useTour } from '@reactour/tour';
 import { useTranslation } from 'react-i18next';
 
 type StepButtonsProps = {
@@ -19,12 +17,6 @@ const StepButtons = ({
   isLast,
 }: StepButtonsProps): JSX.Element => {
   const { t } = useTranslation();
-  const { setIsOpen } = useTour();
-  useEffect(() => {
-    if (!prevDisabled) {
-      setIsOpen(false);
-    }
-  }, [prevDisabled]);
   const nextButtonText = () => {
     if (prevDisabled) {
       return t('start');
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, extendTheme, useColorModeValue } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { StepsStyleConfig as Steps } from 'chakra-ui-steps';
 import { RecoilRoot } from 'recoil';
-import { TourProvider } from '@reactour/tour';
-import { t } from 'i18next';
 
 import App from './screens/Home';
 import reportWebVitals from './reportWebVitals';
@@ -13,43 +11,18 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 const theme = extendTheme({
   config: {
-    initialColorMode: 'dark',
+    initialColorMode: 'system',
+    useSystemColorMode: false,
   },
   components: {
     Steps,
   },
 });
-const steps = [
-  {
-    selector: '.first-step',
-    content: t('tourStepOne'),
-  },
-];
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <RecoilRoot>
-        <TourProvider
-          showBadge={false}
-          showDots={false}
-          showNavigation={false}
-          showPrevNextButtons={false}
-          position="top"
-          styles={{
-            popover: (base) => ({
-              ...base,
-              borderRadius: 5,
-              padding: 30,
-              backgroundColor: useColorModeValue(
-                'rgba(255, 255, 255, 0.8)',
-                'rgba(26, 32, 44, 0.8)',
-              ),
-            }),
-          }}
-          steps={steps}
-        >
-          <App />
-        </TourProvider>
+        <App />
       </RecoilRoot>
     </ChakraProvider>
   </React.StrictMode>,
