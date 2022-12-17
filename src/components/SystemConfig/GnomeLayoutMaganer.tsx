33c9705a9657e819e1243f0d8d8f889dfe54edcd
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-12-17 18:56:00 +0300

    fix: 🐛 showing which layout applied
    showing which gnome layout selected

--- src/components/SystemConfig/GnomeLayoutMaganer.tsx
+++ src/components/SystemConfig/GnomeLayoutMaganer.tsx
@@ -3,7 +3,11 @@ import {
   ModalContent, ModalHeader, ModalOverlay,
   Button, useColorModeValue, ModalFooter, Image, SimpleGrid, Heading, VStack,
 } from '@chakra-ui/react';
-import { useRef, FC } from 'react';
+import { configDir } from '@tauri-apps/api/path';
+import { exists, readTextFile } from '@tauri-apps/api/fs';
+import {
+  useRef, FC, useEffect, useState,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 import { Command } from '@tauri-apps/api/shell';
 
@@ -11,8 +15,23 @@ const GnomeLayoutManager: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const LAYOUTS = ['gnome', 'manjaro', 'material-shell', 'traditional'];
   const { t } = useTranslation();
+  const [isSelected, setIsSelected] = useState('');
   const initialRef = useRef(null);
   const finalRef = useRef(null);
+
+  useEffect(() => {
+    const getLocalData = async () => {
+      const configDirPath = await configDir();
+      const confPath = `${configDirPath}gnome-layout-switcher.conf`;
+      if (await exists(confPath) as unknown as boolean) {
+        const contents = await readTextFile(confPath);
+        const layout = contents.replace('[SETTING]', '').replace(/(\r\n|\n|\s|\r)/gm, '').split('=')[1];
+        setIsSelected(layout);
+        console.log(layout);
+      }
+    };
+    getLocalData();
+  }, []);
   return (
     <>
       <Button
@@ -49,17 +68,19 @@ const GnomeLayoutManager: FC = () => {
                   px={{ base: 2, md: 4 }}
                   height="200px"
                   shadow="xl"
+                  backgroundColor={isSelected === layout ? useColorModeValue('green.100', 'green.900') : 'transparent'}
                   variant="outline"
                   border="1px solid"
                   borderColor={useColorModeValue('gray.800', 'gray.500')}
                   rounded="lg"
                   onClick={async () => {
+                    setIsSelected(layout);
                     new Command('gnome-layout-switcher', [`apply-${layout}`]).execute();
                   }}
                 >
                   <VStack>
                     <Heading as="h5" size="sm" style={{ textTransform: 'capitalize' }}>{layout}</Heading>
-                    <Image src={`${process.env.PUBLIC_URL}/GnomeLayouts/${layout}preview.svg`} alt="Gnome" />
+                    <Image src={`${process.env.PUBLIC_URL}/GnomeLayouts/${layout}preview.svg`} alt={layout} />
                   </VStack>
 
                 </Button>
