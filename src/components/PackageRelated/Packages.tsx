ffbef2216bd7464af650c66425435de8935fd186
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-12-25 12:22:29 +0300

    fix: 🐛 updated arrow colors for package slider
    updated arrow colors for package slider
    
    ✅ Closes: #173

--- src/components/PackageRelated/Packages.tsx
+++ src/components/PackageRelated/Packages.tsx
@@ -6,6 +6,8 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
+  useColorModeValue,
+  Icon,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
@@ -25,23 +27,30 @@ const Arrow = (props: {
   left?: boolean;
   onClick: (e: any) => void;
 }) => {
-  const disabeld = props.disabled ? ' arrow--disabled' : '';
+  const { disabled, left, onClick } = props;
+  const color = useColorModeValue('#181F2E', '#fff');
+  const disColor = '#455A64';
+  const fillColor = disabled ? disColor : color;
   return (
-    <svg
-      onClick={props.onClick}
-      className={`arrow ${
-        props.left ? 'arrow--left' : 'arrow--right'
-      } ${disabeld}`}
+    <Icon
+      onClick={onClick}
+      className={`arrow ${left ? 'arrow--left' : 'arrow--right'}`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      {left && (
+        <path
+          fill={fillColor}
+          d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
+        />
       )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      {!left && (
+        <path
+          fill={fillColor}
+          d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"
+        />
       )}
-    </svg>
+    </Icon>
   );
 };
 const PackagesList: React.FC = () => {
