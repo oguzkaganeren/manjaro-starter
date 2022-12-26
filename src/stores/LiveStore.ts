b783cc474747c5ea0281321ddc8e69fd26ac2a3f
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-12-26 22:09:39 +0300

    fix: 🐛 path fixes

--- src/stores/LiveStore.ts
+++ src/stores/LiveStore.ts
@@ -2,19 +2,20 @@ import {
   atom, selector,
 } from 'recoil';
 import { Command } from '@tauri-apps/api/shell';
+import { invoke } from '@tauri-apps/api/tauri';
 
 export const isLive = selector({
   key: 'isLive',
   get: async () => {
     // do not forget to change path
-    const pathCont = new Command('run_shell_command_with_result', ['[ -d \\"/var/\\" ] && echo \\"true\\"']);
+
     const calamaresCont = new Command('version-control', ['-Q', 'calamares']);
     const cmdVersionResult = await calamaresCont.execute();
     if (cmdVersionResult.stdout) {
-      pathCont.execute().then((response) => {
-        if (response.stdout === 'true') {
+      invoke('run_shell_command_with_result', { command: '[ -d \\"/run/miso/bootmnt/manjaro\\" ] && echo \\"true\\"' }).then((response) => {
+        if (response) {
           return true;
-        }
+        } return false;
       });
     }
     return false;
