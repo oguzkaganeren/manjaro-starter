a2e4307f1fba9c39774f356cc0e07ecefe556b6f
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2022-12-31 15:32:09 +0300

    feat: 🎸 added cancel button for package installation
    added cancel button for package installation

--- src/components/PackageRelated/PackageStatus.tsx
+++ src/components/PackageRelated/PackageStatus.tsx
@@ -1,5 +1,9 @@
 import {
-  useToast, Tooltip, Button,
+  useToast,
+  Tooltip,
+  Button,
+  ButtonGroup,
+  IconButton,
 } from '@chakra-ui/react';
 import React, { useState, ReactNode } from 'react';
 import { RiInstallLine, RiCheckLine } from 'react-icons/ri';
@@ -7,6 +11,7 @@ import { Command } from '@tauri-apps/api/shell';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useTranslation } from 'react-i18next';
 import { info, error } from 'tauri-plugin-log-api';
+import { CloseIcon } from '@chakra-ui/icons';
 import {
   packageState,
 } from '../../stores/PackageStore';
@@ -55,6 +60,17 @@ const PackageStatus: React.FC<PackageStatusProps> = (props) => {
       position: 'bottom-right',
     });
   }
+  const cancelInstall = async (
+    catId:string,
+    pkId:string,
+  ) => {
+    const pack = packageSt.get(catId)?.packages.get(pkId);
+    if (pack?.process) {
+      pack.process.kill();
+      setIsLoadingPackage(new Map(isLoadingPackage?.set(pkId, false)));
+      packageInstallStatusControl(catId, pkId);
+    }
+  };
   const installPackageWithName = async (
     catId:string,
     pkId:string,
@@ -87,7 +103,15 @@ const PackageStatus: React.FC<PackageStatusProps> = (props) => {
       setCommandHistory((prevCommand) => `${prevCommand}\n${line}`);
     });
     const child = await cmd.spawn();
-
+    const pack = packageSt.get(catId)?.packages.get(pkId);
+    const cat = packageSt.get(catId);
+    if (pack) {
+      pack.process = child;
+      cat?.packages.set(pkId, pack);
+      if (cat) {
+        setPackageSt(new Map(packageSt.set(catId, cat)));
+      }
+    }
     info(`pid:${child.pid}`);
   };
   const {
@@ -107,20 +131,29 @@ const PackageStatus: React.FC<PackageStatusProps> = (props) => {
           {t('installed')}
         </Button>
       ) : (
-        <Tooltip label={t('install')}>
-          <Button
-            aria-label="install"
-            flex="1"
-            disabled={!isOnline || isLoadingPackage?.get(pkId)}
-            variant="ghost"
-            leftIcon={<RiInstallLine />}
-            isLoading={isLoadingPackage?.get(pkId) || false}
-            onClick={() => installPackageWithName(catId, pkId, pkgName)}
-            colorScheme="green"
-          >
-            {t('install')}
-          </Button>
-        </Tooltip>
+        <ButtonGroup size="sm" isAttached variant="ghost">
+          <Tooltip label={t('install')}>
+            <Button
+              aria-label="install"
+              flex="1"
+              disabled={!isOnline || isLoadingPackage?.get(pkId)}
+              variant="ghost"
+              leftIcon={<RiInstallLine />}
+              isLoading={isLoadingPackage?.get(pkId) || false}
+              onClick={() => installPackageWithName(catId, pkId, pkgName)}
+              colorScheme="green"
+            >
+              {t('install')}
+            </Button>
+          </Tooltip>
+          {isLoadingPackage?.get(pkId) && (
+          <IconButton
+            aria-label="Cancel"
+            icon={<CloseIcon />}
+            onClick={() => cancelInstall(catId, pkId)}
+          />
+          )}
+        </ButtonGroup>
       )}
     </div>
   );
