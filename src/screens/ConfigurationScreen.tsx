a484dcc17d7e26869dcd29152334f001d6ac2543
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2023-01-20 20:23:40 +0300

    fix: 🐛 removed hdd controller
    removed hdd conroller
    
    ✅ Closes: #286

--- src/screens/ConfigurationScreen.tsx
+++ src/screens/ConfigurationScreen.tsx
@@ -13,8 +13,6 @@ import React, { useState, useEffect } from 'react';
 import { Command } from '@tauri-apps/api/shell';
 import { useTranslation } from 'react-i18next';
 import { useRecoilValue } from 'recoil';
-import { invoke } from '@tauri-apps/api/tauri';
-import { info } from 'tauri-plugin-log-api';
 import KernelComponent from '../components/systemconfig/KernelComponent';
 import SystemInfoComponent from '../components/systemconfig/SystemInfo';
 import SystemUpdate from '../components/systemconfig/SystemUpdate';
@@ -28,7 +26,6 @@ const ConfigurationScreen: React.FC = () => {
   const [isVisibleGnomeLayout, setIsVisibleGnomeLayout] = useState(false);
   const [isVisibleMSM, setIsVisibleMSM] = useState(false);
   const [isVisibleMCP, setIsVisibleMCP] = useState(false);
-  const [isHDD, setIsHDD] = useState(true);
   const isLive = useRecoilValue(liveState);
   const { t } = useTranslation();
   const borderColor = useColorModeValue('gray.800', 'gray.500');
@@ -60,31 +57,6 @@ const ConfigurationScreen: React.FC = () => {
         setIsVisibleMSM(true);
       }
     });
-
-    invoke('run_shell_command_with_result', {
-      command: "lsblk -oMOUNTPOINT,PKNAME -P | grep 'MOUNTPOINT=\"/\"'",
-    }).then((response) => {
-      if (response) {
-        const variables = (response as string).split(' ');
-        const mountNameSpec = variables[1].split('=')[1];
-        const mountName = mountNameSpec.replace(
-          /(\r\n|\n|\s|\r|\\n|\\"|")/gm,
-          '',
-        );
-        info(mountName);
-        if (mountName) {
-          invoke('run_shell_command_with_result', {
-            command: `cat /sys/block/${mountName}/queue/rotational`,
-          }).then((catResponse) => {
-            const isHdd = (catResponse as string).replace(
-              /(\r\n|\n|\s|\r|\\n|\\"|")/gm,
-              '',
-            );
-            setIsHDD(isHdd === '1');
-          });
-        }
-      }
-    });
   });
   return (
     <Tabs
@@ -103,7 +75,7 @@ const ConfigurationScreen: React.FC = () => {
         <Tab>{t('mirrors')}</Tab>
         <Tab>{t('updates')}</Tab>
         {!isLive && <Tab>{t('kernels')}</Tab>}
-        {!isHDD && <Tab>{t('storage')}</Tab>}
+        {!isLive && <Tab>{t('storage')}</Tab>}
         <Tab>{t('settings')}</Tab>
       </TabList>
       <TabPanels>
@@ -121,10 +93,10 @@ const ConfigurationScreen: React.FC = () => {
             <KernelComponent />
           </TabPanel>
         )}
-        {!isHDD && (
-        <TabPanel>
-          <FsTrimServiceComponent />
-        </TabPanel>
+        {!isLive && (
+          <TabPanel>
+            <FsTrimServiceComponent />
+          </TabPanel>
         )}
         <TabPanel>
           <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 5, lg: 8 }}>
