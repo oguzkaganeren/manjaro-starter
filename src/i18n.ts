3d22a97c3c057c607b95bf9e54e91ea8110c2137
Tuxman2 <alan90000@yahoo.com>
2023-01-22 12:51:28 +0000

    Update i18n.ts
    Update i18n.ts for french

---
+++ src/i18n.ts
@@ -0,0 +1,28 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Danish from './translations/da.json';
+import Français from './translation/fr.json';
+import German from './translations/de.json';
+import English from './translations/en.json';
+import Polish from './translations/pl.json';
+import Turkish from './translations/tr.json';
+
+const resources = {
+  Danish,
+  Français
+  German,
+  English,
+  Polish,
+  Turkish,
+};
+
+export const availableLanguages = Object.keys(resources);
+
+i18n.use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+    defaultNS: 'common',
+    fallbackLng: 'English',
+  });
