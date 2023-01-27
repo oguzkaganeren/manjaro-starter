7dd8896ac3414e1c0f993828fd5e651732f506c7
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2023-01-28 01:39:24 +0300

    fix: 🐛 updated social links structure
    ✅ Closes: #315

---
+++ src/components/final/SocialButtons.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+  Text,
+  Button,
+  Center,
+} from '@chakra-ui/react';
+import { open } from '@tauri-apps/api/shell';
+
+import {
+  FiTwitter, FiPackage, FiMail, FiFacebook,
+} from 'react-icons/fi';
+import {
+  SiDiscourse,
+  SiWikipedia,
+  SiGit,
+  SiTelegram,
+  SiReddit,
+  SiBlogger,
+} from 'react-icons/si';
+import { BiDonateHeart } from 'react-icons/bi';
+import { useTranslation } from 'react-i18next';
+// eslint-disable-next-line import/no-named-default
+import { default as SocialDetails } from '../../assets/SocialUrls.json';
+
+interface socialProps {
+  url: string;
+  icon: any;
+  text: string;
+  color: string;
+}
+const icons:any = {
+  FiTwitter: <FiTwitter />,
+  SiDiscourse: <SiDiscourse />,
+  SiWikipedia: <SiWikipedia />,
+  SiGit: <SiGit />,
+  SiTelegram: <SiTelegram />,
+  SiReddit: <SiReddit />,
+  SiBlogger: <SiBlogger />,
+  FiPackage: <FiPackage />,
+  FiMail: <FiMail />,
+  FiFacebook: <FiFacebook />,
+  BiDonateHeart: <BiDonateHeart />,
+};
+const SocialIcon = (props: socialProps) => {
+  const {
+    url, icon, text, color,
+  } = props;
+  const { t } = useTranslation();
+  return (
+    <Button
+      mt={2}
+      mr={2}
+      size="xs"
+      colorScheme={color}
+      onClick={async () => {
+        await open(url);
+      }}
+      leftIcon={icon}
+      shadow="md"
+    >
+      <Center>
+        <Text>{t(text)}</Text>
+      </Center>
+    </Button>
+  );
+};
+const SocialButtons = () => (
+  <>
+    {SocialDetails.detail.map((det) => (
+      <SocialIcon
+        url={det.url}
+        icon={icons[det.icon]}
+        text={det.text}
+        color={det.color}
+      />
+    ))}
+  </>
+);
+
+export default SocialButtons;
--- src/screens/FinalScreen.tsx
+++ src/screens/FinalScreen.tsx
@@ -1,53 +1,17 @@
 import {
-  Container, Heading, Text, Button, Center, Tooltip, IconButton, Spacer,
+  Container, Heading, Text, Tooltip, IconButton, Spacer,
 } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
-import {
-  SiDiscourse, SiWikipedia, SiGit, SiTelegram, SiReddit, SiBlogger,
-} from 'react-icons/si';
-import {
-  FiPackage, FiMail, FiFacebook, FiTwitter,
-} from 'react-icons/fi';
+
 import { GiReturnArrow } from 'react-icons/gi';
-import { open } from '@tauri-apps/api/shell';
-import { BiDonateHeart } from 'react-icons/bi';
 import { useTranslation } from 'react-i18next';
-// eslint-disable-next-line import/no-named-default
-import { default as SocialLinks } from '../assets/SocialUrls.json';
 import SearchComponent from '../components/common/search/SearchComponent';
+import SocialButtons from '../components/final/SocialButtons';
 
 interface FinalProps {
     onReset: () => void;
 }
-interface socialProps {
-  url: string;
-  icon: any;
-  text:string;
-  color:string;
-}
-const SocialIcon = (props: socialProps) => {
-  const {
-    url, icon, text, color,
-  } = props;
-  const { t } = useTranslation();
-  return (
-    <Button
-      mt={2}
-      mr={2}
-      size="xs"
-      colorScheme={color}
-      onClick={async () => {
-        await open(url);
-      }}
-      leftIcon={icon}
-    >
-      <Center>
-        <Text>{t(text)}</Text>
-      </Center>
-    </Button>
-  );
-};
 const FinalScreen: React.FC<FinalProps> = (props) => {
   const { t } = useTranslation();
   const { onReset } = props;
@@ -64,72 +28,7 @@ const FinalScreen: React.FC<FinalProps> = (props) => {
       <Text color="gray.500">{t('readyText')}</Text>
       <Spacer mt={5} />
       <SearchComponent isForPackage={false} />
-      <SocialIcon
-        url={SocialLinks.urls.forum}
-        icon={<SiDiscourse />}
-        text="postForum"
-        color="whatsapp"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.software}
-        icon={<FiPackage />}
-        text="discoverSoftware"
-        color="blue"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.wiki}
-        icon={<SiWikipedia />}
-        text="readWiki"
-        color="orange"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.development}
-        icon={<SiGit />}
-        text="contribute"
-        color="teal"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.telegram}
-        icon={<SiTelegram />}
-        text="telegram"
-        color="telegram"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.facebook}
-        icon={<FiFacebook />}
-        text="facebook"
-        color="facebook"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.twitter}
-        icon={<FiTwitter />}
-        text="twitter"
-        color="twitter"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.reddit}
-        icon={<SiReddit />}
-        text="reddit"
-        color="red"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.blog}
-        icon={<SiBlogger />}
-        text="blog"
-        color="purple"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.mailing}
-        icon={<FiMail />}
-        text="joinMail"
-        color="pink"
-      />
-      <SocialIcon
-        url={SocialLinks.urls.donate}
-        icon={<BiDonateHeart />}
-        text="donate"
-        color="green"
-      />
+      <SocialButtons />
 
       <Tooltip label={t('returnFirstStep')}>
         <IconButton
