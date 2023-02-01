f533abf2b3efcd80e1e419aa0182e45ebe68a897
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2023-02-01 23:28:00 +0300

    fix: 🐛 empty command problem

--- src/stores/CommandStore.ts
+++ src/stores/CommandStore.ts
@@ -2,8 +2,9 @@ import {
   atom,
 } from 'recoil';
 
+const commands: Array<string> = [];
 const commandState = atom({
   key: 'commandState',
-  default: [''],
+  default: commands,
 });
 export default commandState;
