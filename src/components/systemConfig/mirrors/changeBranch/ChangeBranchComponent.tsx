59efcde5a53a22d63c9c590eb40f8dc2ced59871
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2023-02-22 20:12:07 +0300

    refactor: 💡 not completed for now
    ✅ Closes: #230

--- src/components/systemConfig/mirrors/changeBranch/ChangeBranchComponent.tsx
+++ src/components/systemConfig/mirrors/changeBranch/ChangeBranchComponent.tsx
@@ -1,63 +1,104 @@
+import { EditIcon } from '@chakra-ui/icons';
 import {
-  HStack, Select, Text, Tooltip,
+  Select, Text, Tooltip, Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  IconButton,
+  FocusLock,
+  Button,
+  useDisclosure,
+  Stack,
 } from '@chakra-ui/react';
 import { t } from 'i18next';
 import React, { useEffect, useState } from 'react';
 import Branches from '../../../../assets/Braches';
+import ActiveBranchComponent from '../activeBranch/ActiveBranchComponent';
 import { getActiveBranch } from '../MirrorHelper';
 
 function capitalize(str:string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-const ChangeBranchComponent = () => {
+interface FormTypes {
+    onCancel?: () => void,
+}
+
+const ChangeBranchForm = (props:FormTypes) => {
   const [activeBranch, setActiveBranch] = useState('');
   const brValues = Object.values(Branches);
+  const { onCancel } = props;
 
-  const colorSc = () => {
-    switch (activeBranch) {
-      case Branches.Stable:
-        return 'green';
-      case Branches.Testing:
-        return 'orange';
-      case Branches.Unstable:
-        return 'red';
-      default:
-        return 'gray';
-    }
-  };
   useEffect(() => {
     getActiveBranch().then((res) => {
       setActiveBranch(res.stdout);
     });
   }, []);
   return (
-    <HStack>
-      <Text as="b" fontSize="sm" mr={-1} mt={1}>
-        {t('currentBranch')}
-        :
-      </Text>
+    <Stack spacing={4}>
       <Tooltip label={t('activeBranchDesc')}>
-        <Select
-          variant="filled"
-          bg={colorSc()}
-          onChange={() => {
-
-          }}
-        >
-          {brValues.map((name) => (
-            <option
-              value={Branches.Stable}
-              selected={activeBranch === name}
-              autoCapitalize="words"
-            >
-              {capitalize(name)}
-
-            </option>
-          ))}
-
-        </Select>
+        <Text as="b" fontSize="sm" mr={-1} mt={1}>
+          {t('currentBranch')}
+          :
+        </Text>
       </Tooltip>
-    </HStack>
+      <Select
+        size="sm"
+        shadow="base"
+        variant="filled"
+        fontWeight="bold"
+        onChange={() => {
+
+        }}
+      >
+        {brValues.map((name) => (
+          <option
+            value={Branches.Stable}
+            selected={activeBranch === name}
+            autoCapitalize="words"
+          >
+            {capitalize(name)}
+
+          </option>
+        ))}
+
+      </Select>
+      <Button variant="outline" onClick={onCancel}>
+        {t('cancel')}
+      </Button>
+      <Button colorScheme="teal">
+        {t('apply')}
+      </Button>
+    </Stack>
+  );
+};
+
+const ChangeBranchComponent = () => {
+  const { onOpen, onClose, isOpen } = useDisclosure();
+
+  return (
+    <>
+      <ActiveBranchComponent />
+      <Popover
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+        placement="bottom"
+        closeOnBlur={false}
+      >
+        <PopoverTrigger>
+          <IconButton aria-label="edit" size="sm" icon={<EditIcon />} />
+        </PopoverTrigger>
+        <PopoverContent p={5}>
+          <FocusLock persistentFocus={false}>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <ChangeBranchForm onCancel={onClose} />
+          </FocusLock>
+        </PopoverContent>
+
+      </Popover>
+    </>
   );
 };
 
