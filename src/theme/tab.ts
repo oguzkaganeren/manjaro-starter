3dd14b19f87f75a48ddc2240ce2bf9a426d03fa6
Oguz Kagan EREN <me@oguzkaganeren.com.tr>
2023-03-17 22:11:08 +0300

    refactor: 💡 removed unused

--- src/theme/tab.ts
+++ src/theme/tab.ts
@@ -2,49 +2,48 @@
 import { createMultiStyleConfigHelpers } from '@chakra-ui/react';
 import { tabsAnatomy } from '@chakra-ui/anatomy';
 
-const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(tabsAnatomy.keys);
+const {
+  definePartsStyle,
+  defineMultiStyleConfig,
+} = createMultiStyleConfigHelpers(tabsAnatomy.keys);
 // define a custom variant
-const baseStyle = definePartsStyle((props: any) => {
-  const { colorScheme: c } = props; // extract colorScheme from component props
-
-  return {
-    tab: {
-      px: '4',
-      mx: '2',
-      py: '3',
-      rounded: 'md',
-      color: 'gray.700',
-      transition: '.15s ease',
-      _hover: {
-        bg: 'gray.400',
-        _dark: {
-          bg: 'gray.900',
-          color: 'white',
-        },
-        color: 'gray.900',
-        rounded: 'md',
-      },
+const baseStyle = definePartsStyle(() => ({
+  tab: {
+    px: '4',
+    mx: '2',
+    py: '3',
+    rounded: 'md',
+    color: 'gray.700',
+    transition: '.15s ease',
+    _hover: {
+      bg: 'gray.400',
       _dark: {
-        color: 'gray.400',
-      },
-      justifyContent: 'flex-start',
-      fontWeight: 'semibold',
-      // use colorScheme to change background color with dark and light mode options
-      _selected: {
-        bg: 'green.600',
-        rounded: 'md',
-        _dark: {
-          bg: 'green.500',
-          color: 'white',
-        },
+        bg: 'gray.900',
         color: 'white',
       },
+      color: 'gray.900',
+      rounded: 'md',
     },
-    tabpanel: {
-      pt: 0,
+    _dark: {
+      color: 'gray.400',
     },
-  };
-});
+    justifyContent: 'flex-start',
+    fontWeight: 'semibold',
+    // use colorScheme to change background color with dark and light mode options
+    _selected: {
+      bg: 'green.600',
+      rounded: 'md',
+      _dark: {
+        bg: 'green.500',
+        color: 'white',
+      },
+      color: 'white',
+    },
+  },
+  tabpanel: {
+    pt: 0,
+  },
+}));
 
-// export the component theme
-export const tabsTheme = defineMultiStyleConfig({ baseStyle });
+const tabsTheme = defineMultiStyleConfig({ baseStyle });
+export default tabsTheme;
